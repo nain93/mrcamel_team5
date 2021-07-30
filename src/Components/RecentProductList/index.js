cab569f40ebd770e9edfc97ea8d2f746ed536993
Park <pjane123@naver.com>
2021-07-30 21:13:13 +0900

    add: RecentProductList 컴포넌트 로직 추가
    - RecentProduct 옵션용 RecentProductFilter 추가
    - RecentProductFilter내에 브랜드 및 ignore필터용 CheckBoxFilter, 정렬용 SortFilter 추가

--- src/Components/RecentProductList/index.js
+++ src/Components/RecentProductList/index.js
@@ -1,7 +1,31 @@
 import React, { Component } from 'react';
+import styled from 'styled-components';
+import RecentProduct from './RecentProduct';
+import RecentProductFilter from './RecentProductFilter';
+
+const ProductListContainer = styled.div``;
+const Container = styled.div``;
 
 export default class index extends Component {
+  constructor(props) {
+    super(props);
+    const { productList, ignoreList, brandList } = this.props;
+    this.state = {
+      productList: productList,
+      ignoreList: ignoreList,
+      brandList: brandList,
+    };
+  }
+
   render() {
-    return <div></div>;
+    const { productList, ignoreList, brandList } = this.state;
+    return (
+      <Container>
+        <ProductListContainer>{productList.map((item) => {
+          return <RecentProduct item = {item}/>
+
+        })}</ProductListContainer>
+      </Container>
+    );
   }
 }
