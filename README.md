35b76725a4c47855749e2e4f35ac990a4bd6e251
git-k <kgunu93@gmail.com>
2021-07-30 20:57:34 +0900

    modify : app 및 utils 구조

---
+++ README.md
@@ -0,0 +1,32 @@
+# MRCAMEL_TEAMS-1조
+
+## Getting Started
+
+1. Clone the repo
+   
+2. Install NPM packages
+   ```sh
+   npm install
+   ```
+3. Start
+   ```sh
+   npm start
+   ```
+
+## Reference
+
+```
+ 
+```
+
+App.js
+
+## Use
+
+- CRA(create-react-app)
+- react
+- react-router-dom
+- styled-components
+- styled-reset
+
+
--- src/App.js
+++ src/App.js
@@ -5,7 +5,8 @@ import RecentList from "pages/RecentList";
 import React, { Component } from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import styled from "styled-components";
-import GlobalStyles from "./styles/GlobalStyles";
+import GlobalStyles from "styles/GlobalStyles";
+import {getProducts} from 'utils/api/api'
 
 const GlobalContainer = styled.div`
   max-width: 768px;
@@ -16,16 +17,26 @@ const GlobalContainer = styled.div`
 `;
 
 export default class App extends Component {
+  state = {
+    products: [],
+  }
+
+  componentDidMount() {
+    getProducts().then(products => this.setState({ products }));
+  }
+
   render() {
+    const {products} = this.state;
+
     return (
       <GlobalContainer>
         <Router>
           <GlobalStyles />
           <Nav />
           <Switch>
-            <Route path="/" exact component={Home} />
-            <Route path="/product" component={Product} />
-            <Route path="/recentlist" component={RecentList} />
+            <Route exact path="/" component={props => <Home {...props} products={products} />}/>
+            <Route exact path="/product" component={props => <Product {...props} products={products} />} />
+            <Route exact path="/recentlist" component={RecentList} />
           </Switch>
         </Router>
       </GlobalContainer>
