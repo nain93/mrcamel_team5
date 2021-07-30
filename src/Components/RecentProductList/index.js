d25979e34c6cc83f620a740886cacdef4412a1c5
Park <pjane123@naver.com>
2021-07-30 21:58:52 +0900

    add: RecentProduct filterOptions 로직 구현

--- src/Components/RecentProductList/index.js
+++ src/Components/RecentProductList/index.js
@@ -14,17 +14,41 @@ export default class index extends Component {
       productList: productList,
       ignoreList: ignoreList,
       brandList: brandList,
+      filterOptions: {
+        sort: 'NEWEST', // or LOWPRICE
+        ignore: true,
+        brands: ['나이키'],
+      },
     };
   }
 
   render() {
-    const { productList, ignoreList, brandList } = this.state;
+    const { productList, ignoreList, brandList, filterOptions } = this.state;
+    const ignoreIdList = ignoreList.map((item) => item.id);
     return (
       <Container>
-        <ProductListContainer>{productList.map((item) => {
-          return <RecentProduct item = {item}/>
-
-        })}</ProductListContainer>
+        <RecentProductFilter brandList={brandList} filter={filterOptions} />
+        <ProductListContainer>
+          {productList
+            .filter((item) => {
+              if (filterOptions.ignore) {
+                return !ignoreIdList.includes(item.id);
+              }
+              return true;
+            })
+            .filter((item)=>{
+              if(filterOptions.brands.length){
+                return !filterOptions.brands.includes(item.brand);
+              }
+              return true;
+            })
+            .sort((a, b) => {
+              return filterOptions.sort === 'NEWEST' ? new Date(b.date) - new Date(a.date) : b.price - a.price;
+            })
+            .map((item) => {
+              return <RecentProduct item={item} key={item.id} />;
+            })}
+        </ProductListContainer>
       </Container>
     );
   }
