cc74b7e25639807fbc6350bb0553f2109b819fc7
Park <pjane123@naver.com>
2021-07-30 21:26:21 +0900

    add: mocking용 localStorage 설정 함수 추가

--- src/pages/RecentList.js
+++ src/pages/RecentList.js
@@ -1,25 +1,27 @@
 import React, { Component } from 'react';
 import RecentProductList from 'Components/RecentProductList';
-import { getLocalStorge, setLocalStorage } from 'utils/common';
 import styled from 'styled-components';
+
+const getLocalStorge = (key) => {
+  return JSON.parse(localStorage.getItem(key));
+};
+
+const setLocalStorage = (key, input) => {
+  localStorage.setItem(key, JSON.stringify(input));
+};
 
 const Container = styled.div`
   height: calc(100vh - 57px);
 `;
 
-const mockBrandList = [
-  '나이키',
-  '스톤아일랜드',
-  '구찌',
-  '루이비통'
-]
+const mockBrandList = ['나이키', '스톤아일랜드', '구찌', '루이비통'];
 
 const mockList = [
-  { title: '나이키 x 스투시 빅로고 커스텀 스우시 에센셜 후디 후드티 올드스쿨', brand: '나이키', price: 45000, id: 83, date: '20210730202942'},
-  { title: '스톤아일랜드 와펜패치 라미플록포켓 맨투맨 블랙!', brand: '스톤아일랜드', price: 310000, id: 84, date: '20210730202941'},
+  { title: '나이키 x 스투시 빅로고 커스텀 스우시 에센셜 후디 후드티 올드스쿨', brand: '나이키', price: 45000, id: 83, date: '20210730202942' },
+  { title: '스톤아일랜드 와펜패치 라미플록포켓 맨투맨 블랙!', brand: '스톤아일랜드', price: 310000, id: 84, date: '20210730202941' },
   { title: '나이키 바람막이', brand: '나이키', price: 81000, id: 85, date: '20210730202947' },
   { title: '나이키 대한민국 농구 국가대표팀 숏슬리브 반팔 티셔츠 드라이핏', brand: '나이키', price: 50000, id: 86, date: '20210730202111' },
-  { title: '나이키 드라이핏 라운드넥 긴팔 티셔츠 러닝 런닝 헬스 운동', brand: '나이키', price: 45000, id: 87, date: '20210730202945'  },
+  { title: '나이키 드라이핏 라운드넥 긴팔 티셔츠 러닝 런닝 헬스 운동', brand: '나이키', price: 45000, id: 87, date: '20210730202945' },
   { title: '스톤아일랜드 패딩조끼(상태 최상)네이비', brand: '스톤아일랜드', price: 400000, id: 89, date: '20210730202944' },
   { title: '(정품) 구찌 마몬트 카드지갑', brand: '구찌', price: 270000, id: 90, date: '20210730202942' },
   { title: '루이비통 반바지 판매합니다', brand: '루이비통', price: 700000, id: 91, date: '20210730202949' },
@@ -28,23 +30,23 @@ const mockList = [
 export default class RecentList extends Component {
   constructor() {
     super();
-    // NEED TO REMOVE ON MERGE 
+    // NEED TO REMOVE ON MERGE
     setLocalStorage('resentList', mockList);
-    setLocalStorage('ignoreList', mockList.slice(0,3));
+    setLocalStorage('ignoreList', mockList.slice(0, 3));
     // REMOVE
     this.state = {
       resentList: getLocalStorge('resentList'),
       ignoreList: getLocalStorge('ignoreList'),
     };
   }
-  componentDidMount(){
+  componentDidMount() {
     console.log(this.state.resentList);
     console.log(this.state.ignoreList);
   }
   render() {
     return (
       <Container>
-        <RecentProductList productList ={this.state.resentList} ignoreList={this.state.ignoreList} brandList={mockBrandList} />
+        <RecentProductList productList={this.state.resentList} ignoreList={this.state.ignoreList} brandList={mockBrandList} />
       </Container>
     );
   }
